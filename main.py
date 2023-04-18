759e96ceaa8187bb3f09001e7e419ef7cfd88898
Noussiie <snoussi2000sn@gmail.com>
2023-04-18 23:03:37 +0000

    fix clear cmd

--- main.py
+++ main.py
@@ -6,18 +6,12 @@ intents.message_content=True
 bot=commands.Bot(command_prefix='/',intents=intents)
 
 
-
+#sucess log message-terminal
 @bot.event
 async def on_ready():
     print("Logged on as", bot.user)
 
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send('https://tenor.com/bUrwp.gif')
-
-
-
+# messages replys
 @bot.event
 async def on_message(message):
     if message.author==bot.user:
@@ -33,6 +27,33 @@ async def on_message(message):
         await message.channel.send('bekher hmdlh')
         
     await bot.process_commands(message)
+
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('https://tenor.com/bUrwp.gif')
+
+@bot.command()
+async def pingd(ctx):
+    await ctx.send(bot.latency)
+
+
+# Admin commands
+@bot.command()
+@commands.has_permissions(administrator=True)
+async def mse7(ctx , amount:int=None):
+    if(not amount):
+        await ctx.send("3emer l'amount libaghi tmse7 ")
+        return
+    elif(amount>10):
+        await ctx.send("Max houwa 10 characters ")
+        return
+    # amount +1 to delete the /cmd itself  ex: /clear 5 => 5msgs+1cmd     
+    await ctx.channel.purge(limit=amount+1)     
+
+
+
 
 
 bot.run('MTA1ODM5MzE1MTY4MjkwODE2MA.GOUA2E.O7DOE-TuY0KBtNgRyX8mK7V3fJf3BtSfVDWJlk')
